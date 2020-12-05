bc5c4d46d21f303c3aaae834dc9c9c923d00c00c
arnoantartica <36259729+arnoantartica@users.noreply.github.com>
2020-12-05 14:45:38 +0300

    start

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include('include/header.php'); ?>
+
+<main id="intro">
+
+    <aside>
+        <!-- logo top -->
+        <img src="img/logo-archybald-top.png" alt="archybald" class="anim-1">
+        <!-- logo bottom -->
+        <img src="img/logo-archybald-bottom.png" alt="archybald" class="anim-3">
+        <!-- explore -->
+        <div class="explore anim-2">
+            <a href="biens.php">
+                <button data-lang="Explore">explore</button>
+            </a>
+        </div>
+    </aside>
+
+    <!-- swiper -->
+    <section class="swiper-container swiper-container-intro">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide intro-1"></div>
+            <div class="swiper-slide intro-2"></div>
+            <div class="swiper-slide intro-3"></div>
+        </div>
+        <div class="swiper-pagination"></div>
+    </section>
+    <!-- swiper END -->
+
+
+</main>
+
+
+<?php include('include/footer.php'); ?>
